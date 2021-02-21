6aaa47fc36c742da40c369cf428ee8863873712f
Kirillova <keira.kirillova@gmail.com>
2021-02-21 20:45:56 +0200

    get contacts data

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,45 @@
+import { useState, useEffect } from 'react';
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+      } catch(e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getContacts();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError
+  }
+}
+
 export const Contacts = () => {
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return <div>is loading...</div>
+  }
+
+  if (contacts.isError) {
+    return <div>Error!</div>
+  }
+
   return <div>Contacts</div>
 }
